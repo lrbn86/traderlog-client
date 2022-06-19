17829218b11300601e227f696301192df303eeb3
lrbn86 <lrbn86@gmail.com>
2022-06-19 12:10:10 -0400

    Add HTTPClientModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
+
 import { AppComponent } from './app.component';
 import { PerformanceStatsComponent } from './components/performance-stats/performance-stats.component';
 import { TradesListComponent } from './components/trades-list/trades-list.component';
@@ -16,7 +17,8 @@ import { AddTradeComponent } from './components/add-trade/add-trade.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
